8cbb5c179432b94231abb06013449340f9ccded5
“Thairat <thairat.st@gmail.com>
2026-01-08 11:20:13 +0700

    widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,36 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: Text('Basic Widget'),
+          backgroundColor: Color.fromARGB(255, 104, 125, 231),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage('asset/ass1.gif'),
+                width: MediaQuery.of(context).size.width * 0.5,
+              ),
+              Text('Hello World!'),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('Floating Action Button Pressed');
+          },
+          child: Text('Click me'),
+          backgroundColor: Color.fromARGB(255, 227, 157, 60),
+          foregroundColor: Colors.white,
         ),
       ),
     );
